b2b9df4cc4e125300d76ac8bccb7e4b89d54b00b
bananabread08 <rmkfermin@gmail.com>
2023-11-09 17:57:44 +0800

    add SESSION_SECRET

--- server/src/config.ts
+++ server/src/config.ts
@@ -6,7 +6,7 @@ export const PORT = process.env.PORT || '3003';
 export const WEBSITE_URL =
   process.env.WEBSITE_URL || `https://localhost:${PORT}`;
 const allowedOrigins = [WEBSITE_URL];
-
+export const SESSION_SECRET = process.env.SESSION_SECRET || 'liham_api_secret';
 export const corsOptions: cors.CorsOptions = {
   origin: allowedOrigins,
   credentials: true,
