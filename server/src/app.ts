9e4858f670ecd2498dc0770901324312c065296b
bananabread08 <rmkfermin@gmail.com>
2023-11-09 17:58:50 +0800

    create passport middlewares

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,11 +1,19 @@
-import express from 'express';
+import 'express-async-errors';
+import express, { NextFunction } from 'express';
 import morgan from 'morgan';
 import cors from 'cors';
 import { corsOptions } from './config';
-import path from 'path';
+// import path from 'path';
 import { Request, Response } from 'express';
 import * as middlewares from './middlewares/middlewares';
+import 'express-async-errors';
+import passport from 'passport';
+import session from 'express-session';
+import { authRouter } from './routes/auth.route';
+import connectPgSimple from 'connect-pg-simple';
+import { SESSION_SECRET } from './config';
 
+const store = new (connectPgSimple(session))();
 const app = express();
 
 app.use(morgan('dev'));
@@ -14,19 +22,44 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 // app.use(compression());
 // app.use(helmet());
+app.use(
+  session({
+    store: store,
+    secret: SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+      secure: process.env.NODE_ENV === 'production' ? true : false,
+      httpOnly: true,
+      sameSite: 'none',
+      maxAge: 1000 * 60 * 60 * 24,
+    },
+  }),
+);
+app.use(passport.initialize());
+app.use(passport.session());
 
-if (process.env.NODE_ENV === 'production') {
-  const __dirname = path.resolve();
-  app.use(express.static(path.join(__dirname, 'dist')));
+passport.use(middlewares.passportHandler);
+passport.serializeUser(middlewares.serializeUser);
+passport.deserializeUser(middlewares.deserializeUser);
 
-  app.get('*', (_req: Request, res: Response) =>
-    res.sendFile(path.resolve(__dirname, 'dist', 'index.html')),
-  );
-} else {
-  app.get('/', (_req, res) => {
-    res.json({ message: 'welcome to liham api' });
-  });
-}
+app.get('/api/v1/demo', (req: Request, res: Response, _next: NextFunction) => {
+  res.json({ sessionID: req.sessionID });
+});
+
+app.use('/api/v1/auth', authRouter);
+// if (process.env.NODE_ENV === 'production') {
+//   const __dirname = path.resolve();
+//   app.use(express.static(path.join(__dirname, 'dist')));
+
+//   app.get('*', (_req: Request, res: Response) =>
+//     res.sendFile(path.resolve(__dirname, 'dist', 'index.html')),
+//   );
+// } else {
+//   app.get('/', (_req, res) => {
+//     res.json({ message: 'welcome to liham api' });
+//   });
+// }
 
 app.use(middlewares.unknownEndpoint);
 app.use(middlewares.errorHandler);
--- server/src/middlewares/middlewares.ts
+++ server/src/middlewares/middlewares.ts
@@ -1,5 +1,9 @@
 import { Request, Response, NextFunction, ErrorRequestHandler } from 'express';
 import createHttpError, { isHttpError } from 'http-errors';
+import { Strategy as LocalStrategy } from 'passport-local';
+import { db } from '../utils/db';
+import bcrypt from 'bcrypt';
+
 export const unknownEndpoint = (
   req: Request,
   res: Response,
@@ -30,4 +34,48 @@ export const errorHandler: ErrorRequestHandler = (
   }
   res.status(status).json({ error: errorMessage });
   next();
+};
+
+export const passportHandler = new LocalStrategy(async function (
+  username,
+  password,
+  done,
+) {
+  try {
+    const user = await db.user.findUnique({ where: { username } });
+    if (!user) return done(null, false);
+    const passwordMatch = bcrypt.compare(password, user.password);
+    if (!passwordMatch) return done(null, false);
+    return done(null, {
+      id: user.id,
+      username: user.username,
+      firstName: user.firstName,
+      lastName: user.lastName,
+      avatar: user.avatar,
+    });
+  } catch (err) {
+    return done(err);
+  }
+});
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export const serializeUser = (user: any, cb: any) => {
+  cb(null, user.id);
+};
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export const deserializeUser = async (id: number, cb: any) => {
+  try {
+    const user = await db.user.findUnique({ where: { id } });
+    if (user)
+      cb(null, {
+        id: user.id,
+        username: user.username,
+        firstName: user.firstName,
+        lastName: user.lastName,
+        avatar: user.avatar,
+      });
+  } catch (error) {
+    cb(error);
+  }
 };
