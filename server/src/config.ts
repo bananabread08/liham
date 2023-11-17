5360cdfad7878fb50930215c1ace703151973a0c
bananabread08 <rmkfermin@gmail.com>
2023-11-17 18:36:19 +0800

    merge Express User type w/ custom user & update tsconfig w/ typeroots

--- server/src/config.ts
+++ server/src/config.ts
@@ -5,7 +5,7 @@ import cors from 'cors';
 export const PORT = process.env.PORT || '3003';
 export const WEBSITE_URL =
   process.env.WEBSITE_URL || `https://localhost:${PORT}`;
-const allowedOrigins = [WEBSITE_URL];
+const allowedOrigins = WEBSITE_URL;
 export const SESSION_SECRET = process.env.SESSION_SECRET || 'liham_api_secret';
 export const corsOptions: cors.CorsOptions = {
   origin: allowedOrigins,
---
+++ server/src/types/types.ts
@@ -0,0 +1,7 @@
+export interface PublicUser {
+  id: number;
+  username: string;
+  firstName: string | null;
+  lastName: string | null;
+  avatar: string | null;
+}
