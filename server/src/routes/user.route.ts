013e6a51a99f8e40ad156e3c6545bff6e1368f88
bananabread08 <rmkfermin@gmail.com>
2023-12-08 19:10:24 +0800

    refactor: change the scope of the returned user obj to only id and username

--- server/src/routes/user.route.ts
+++ server/src/routes/user.route.ts
@@ -1,3 +1,4 @@
+import { getUserContacts } from './../controllers/user.controller';
 import express from 'express';
 import {
   addToContacts,
@@ -10,6 +11,7 @@ import {
 export const userRouter = express.Router();
 
 userRouter.get('/currentUser', getCurrentUser);
+userRouter.get('/currentUser/contacts', getUserContacts);
 userRouter.patch('/:userId/add/contacts', addToContacts);
 userRouter.patch('/:userId/remove/contacts', removeFromContacts);
 userRouter.patch('/:userId', updateProfile);
